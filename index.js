d6cd6b969b2ef09e7104246d6cf46711c68cf5f2
arulkumar <arulkumar10112001@gmail.com>
2026-01-01 16:59:32 +0530

    New update

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const app = express();
 // use middleware
 app.use(express.json());
 app.use(cors({
-    origin:"https://arul-frontend.netlify.app/"
+    origin:"https://arul-frontend.netlify.app",
+    methods:["GET","POST", "PUT", "DELETE"],
+    credentials: true
 }));
 app.use("/api", userRoutes);
 app.use("/auth", authRoutes);
